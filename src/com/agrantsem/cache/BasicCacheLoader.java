fab6c374df02e06a040aa2e877b6099244d1ad90
zheng fei <fly9i@zhengmatoMacBook-Air.local>
2012-12-24 03:05:30 +0800

    修改cacheloader部分，自动重载cache

--- src/com/agrantsem/cache/BasicCacheLoader.java
+++ src/com/agrantsem/cache/BasicCacheLoader.java
@@ -1,5 +1,8 @@
 package com.agrantsem.cache;
 
-public abstract class BasicCacheLoader implements Runnable{
-
+public abstract class BasicCacheLoader<K,V> implements Runnable{
+	protected SimpleCache<K, V> cache=null;
+	public BasicCacheLoader(SimpleCache<K, V> cache){
+		this.cache=cache;
+	}
 }
--- src/com/agrantsem/cache/SimpleCache.java
+++ src/com/agrantsem/cache/SimpleCache.java
@@ -3,12 +3,14 @@ package com.agrantsem.cache;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.text.html.MinimalHTMLWriter;
+
 public class SimpleCache<K,V> {
 	private String name;
 	private Map<K,V> cacheMap=new HashMap<K, V>();
 	private CacheConfiguration config=null;
 	private long lastUpdate=0;
-	private Class<Runnable> classLoader;
+
 	public SimpleCache(CacheConfiguration config,String name){
 		this.name=name;
 		this.config=config;
@@ -17,7 +19,41 @@ public class SimpleCache<K,V> {
 		this.cacheMap.put(key, value);
 		this.lastUpdate=System.currentTimeMillis();
 	}
+	public synchronized void update(Map<K, V> cache){
+		this.cacheMap=cache;
+	}
+	private void updateAll(){
+		long delay=System.currentTimeMillis()-lastUpdate;
+		if(delay<this.config.getMaxTimeOut()){
+			try {
+				Runnable run=this.config.getLoader().newInstance();
+				Thread t=new Thread(run);
+				t.start();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else if(delay>=this.config.getMaxTimeOut()){
+			try {
+				Runnable run=this.config.getLoader().newInstance();
+				run.run();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 	public synchronized V get(K key){
+		long delay=System.currentTimeMillis()-lastUpdate;
+		if(delay>=this.config.getMinTimeOut()){
+			
+		}
 		return this.cacheMap.get(key);
 	}
 	
